dc80c2bd0ec36cf894d3d2ff43ab0a7e27beab06
ravi kumar dhawan <ravidhawan8899@gmail.com>
2022-12-16 23:52:44 +0530

    resume fontend done

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log(`hello remaker's`);
+
+
+function addNewWEField() {
+  let newNode = document.createElement("textarea");
+  newNode.classList.add("form-control");
+  newNode.classList.add("weField");
+  newNode.classList.add("mt-2");
+  newNode.setAttribute("rows", 3);
+  newNode.setAttribute("placeholder", "Enter here");
+  let weOb = document.getElementById("we");
+  let weAddButton = document.getElementById("weAddButton");
+  weOb.insertBefore(newNode, weAddButton);
+}
+function addNewAQField() {
+  let newNode = document.createElement("textarea");
+  newNode.classList.add("form-control");
+  newNode.classList.add("eqField");
+  newNode.classList.add("mt-2");
+  newNode.setAttribute("rows", 3);
+  newNode.setAttribute("placeholder", "Enter here");
+  let aqOb = document.getElementById("aq");
+  let aqAddButton = document.getElementById("aqAddButton");
+  aqOb.insertBefore(newNode, aqAddButton);
+}
